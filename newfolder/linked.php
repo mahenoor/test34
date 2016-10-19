9b78ed33d9350c7a88411f57175fdc9435eb21e1
mahenoor <mahe.noor@compassitesinc.com>
2016-10-19 17:19:03 +0530

    Add files via upload

---
+++ newfolder/linked.php
@@ -0,0 +1,15 @@
+<!DOCTYPE version = 1.1 encoding = "UTF-8"?>
+<html>
+<head>
+<title>doc</title>
+</head>
+<ul>
+<?php
+foreach($person as $key=>$feature)
+{
+	echo "<li>$key</li>";
+	echo "<li>$feature</li>";
+}
+?>
+</ul>
+</html>
